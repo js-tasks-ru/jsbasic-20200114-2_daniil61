cc367ff712f5a8ede653ac7e851036d6d87c0213
bigxxxg <dp070593gda@gmail.com>
2020-01-31 15:04:29 +0300

    1-module task 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,10 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+    if(str.length > maxlength) {
+       return str.slice(0,maxlength) +'…';
+    } else {
+        return str;
+    }
 
 }
