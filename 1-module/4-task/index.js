a5be0a9f800b02c4100f709573fc9822f1e900b1
bigxxxg <dp070593gda@gmail.com>
2020-02-03 19:32:51 +0300

    test commit

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -8,7 +8,6 @@ function checkSpam(str) {
     const arr = ['1XbeT now', 'free xxxxx'];
     arr.forEach( function( item, i, arr ) {
 
-
         if(str.toLowerCase().indexOf(item.toLowerCase()) >= 0) {
             result = true;
         }
