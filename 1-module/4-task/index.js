8caed7ae088ebc7e9c91227acac3018ccc90e40d
bigxxxg <dp070593gda@gmail.com>
2020-02-03 19:34:06 +0300

    правки

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -7,7 +7,6 @@ function checkSpam(str) {
     let result = false;
     const arr = ['1XbeT now', 'free xxxxx'];
     arr.forEach( function( item, i, arr ) {
-
         if(str.toLowerCase().indexOf(item.toLowerCase()) >= 0) {
             result = true;
         }
